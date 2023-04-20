9bba63046d2dbfa70b9edcb5042e03020f10c1da
Lumenatom <lumenatom@gmail.com>
2023-04-20 21:04:31 +0300

    add library styled, normalized,

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base:'/McDonalds_Application'
+    plugins: [react()
+    ],
+    base: '/McDonalds_Application',
 })
